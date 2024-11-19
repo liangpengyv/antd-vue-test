f22db0f85600a5d157e96218dac0257b6ef2e64c
liangpengyv <me@laoliang.ink>
2024-11-19 17:52:45 +0800

    test: 测试 vue3 无限滚动组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
-  <div>
-    <a-flex gap="24">
-      <a-button>按钮</a-button>
-      <a-button>按钮</a-button>
-      <a-button>按钮</a-button>
-    </a-flex>
-  </div>
+  <vue-seamless-scroll class="scroll" :list="list" :hover="true">
+    <div class="item" v-for="(item, index) in list" :key="index">
+      <span>{{ item.title }}</span>
+      <span>{{ item.date }}</span>
+    </div>
+  </vue-seamless-scroll>
 </template>
 
 <script setup>
-import { Button as AButton, Flex as AFlex } from 'ant-design-vue'
+import { ref } from 'vue'
+import { Vue3SeamlessScroll as VueSeamlessScroll } from 'vue3-seamless-scroll'
+
+const list = ref([
+  {
+    title: 'Vue 无缝滚动组件展示数据第1条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第2条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第3条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第4条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第5条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第6条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第7条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第8条',
+    date: Date.now(),
+  },
+  {
+    title: 'Vue 无缝滚动组件展示数据第9条',
+    date: Date.now(),
+  },
+])
 </script>
 
-<style scoped></style>
+<style scoped>
+.scroll {
+  height: 270px;
+  width: 500px;
+  margin: 100px auto;
+  overflow: hidden;
+}
+
+.scroll .item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3px 0;
+}
+</style>
